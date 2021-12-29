1cb039ece6eea52bc2ab0d059d754b79edc78ed2
Philipp Klaeger <80161087+philippklaeger@users.noreply.github.com>
2021-12-29 21:30:51 +0100

    Add files via upload

---
+++ data/data_sets_download.R
@@ -0,0 +1,26 @@
+###########################################################
+#                 D A T A  S E T S                        #
+###########################################################
+
+
+
+# Download Links for Supervisors
+# All data sets are in CSV format
+
+
+###########################################################
+
+
+# 1. Cleaned Data Sets - Before Analysis
+## Political Marketing
+url1 <- "https://drive.google.com/file/d/1BtQ1bhOUISmhqDY-fRcXS0X4WJOIXxtL/view?usp=sharing"
+
+## User-Generated Discussions
+url2 <- "https://drive.google.com/file/d/1f_QpmKQt2yiO-InzM41nmzIUZBVcgWSw/view?usp=sharing"
+
+# 2. Cleaned Data Sets - After Analysis
+## Marketing
+url3 <- "https://drive.google.com/file/d/1wRvswelFP5wBGvSuMUZUYEEXA4HFxY-j/view?usp=sharing"
+
+## Users
+url4 <- "https://drive.google.com/file/d/19eIIgpT3X3k-IXmUdI1dFAkNhccNjYDW/view?usp=sharing"
